ac55199ab1605266807bf5e1a7efb740cef88920
jonathanpv <jonpad512@gmail.com>
2023-09-27 01:59:43 -0500

    integrate ai tutor with retrieval steps shown to user

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,28 +1,143 @@
 // ./app/api/chat/route.ts
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || '',
-});
+import { OpenAIStream, StreamingTextResponse } from 'ai';  // Importing the missing functions
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai'; // Static import
+import { OperatingSystemsRole, OperatingSystemsRetrievalRole } from './aitutors';
+import { QueryGeneratorMessages } from './queryGenerator';
 
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
 
-export async function POST(req: Request) {
+// Use named export for POST method
+export async function POST(req, res) {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
+  let role = [ OperatingSystemsRole ];
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages: messages,
+  let msg = role.concat(messages);
+  // Azure talk to your data setup
+  const endpoint = process.env.ENDPOINT;
+  // Your Azure OpenAI API key
+  const azureApiKey = process.env.AZURE_API_KEY;
+  // Your Azure Cognitive Search endpoint, admin key, and index name
+  const azureSearchEndpoint = process.env.AZURE_SEARCH_ENDPOINT;
+  const azureSearchAdminKey = process.env.AZURE_SEARCH_KEY;
+  const azureSearchIndexName = process.env.AZURE_SEARCH_INDEX;
+
+  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+
+  // get intents to search
+  const intentDeploymentId = "plutolearning-gpt-35"
+  QueryGeneratorMessages.push(messages[messages.length - 1]);
+  const intentMessages = QueryGeneratorMessages;
+  const intentEvents = client.listChatCompletions(intentDeploymentId, intentMessages);
+  let intents;
+
+  const deploymentId = "plutolearning-gpt4";
+
+  const encoder = new TextEncoder();
+  const stream = new ReadableStream({
+    async start(controller) {
+      let buffer = "";
+      // step 1: the generate queries 
+      controller.enqueue(encoder.encode("Searching for...\n\n"));
+      for await (const event of intentEvents) {
+        for (const choice of event.choices) {
+          let content = choice.delta?.content;
+          if (content !== null && content !== undefined) {
+            const uint8Array = encoder.encode(content);
+            buffer += content;
+            controller.enqueue(uint8Array);
+          }
+        }
+      }
+
+      // step 2: data retrieval
+      let queries;
+      if (buffer[0] == "[" && buffer[buffer.length - 1] == "]") {
+        queries = JSON.parse(buffer);
+      }
+      let rawcontext = [];
+      let context = [];
+      for (const query of queries) {
+        let querymessages = [
+          { role: "user", content: query }
+        ]
+        controller.enqueue(encoder.encode(`\nretrieving documents for: ${query}\n`));
+        const result = await client.getChatCompletions("plutolearning-gpt-35-turbo-16k", querymessages, {
+          maxTokens: 200,
+          azureExtensionOptions: {
+            extensions: [
+              {
+                type: "AzureCognitiveSearch",
+                parameters: {
+                  endpoint: azureSearchEndpoint,
+                  key: azureSearchAdminKey,
+                  indexName: azureSearchIndexName,
+                  topNDocuments: 10,
+                  inScope: true,
+                  roleInformation: OperatingSystemsRetrievalRole,
+                },
+              },
+            ],
+          },
+        });
+        for (const choice of result.choices) {
+          rawcontext.push(choice.message?.context?.messages[0].content);
+        }
+      }
+
+      // Summarize shit
+      let summarizeMessages = [
+        {role: "system", content: `Don't include URLs. Summarize the following without losing details. When summarizing the provided information, extract and relay specific and crucial details such as exact dates, names, topics, and any numeric information. Do not refer to the information source as a "citation", "document", or "link". Include explicit dates when mentioned, list specific questions or topics, and provide a concise representation of tables or lists present in the information. Prioritize the accuracy and completeness of the information over brevity, maintaining coherence and readability. Avoid any meta-commentary about the information source.`},
+        {role: "user", content: rawcontext.join("\n") },
+      ]
+
+      const summarizeResult = client.listChatCompletions("plutolearning-gpt-35-turbo-16k", summarizeMessages);
+      controller.enqueue(encoder.encode("summarizing citations...\n\n"));
+
+      let contextMessage = "\nEmbedded Context:\n\n";
+      for await (const event of summarizeResult) {
+        for (const choice of event.choices) {
+          let content = choice.delta?.content;
+          if (content !== null && content !== undefined) {
+            const uint8Array = encoder.encode(content);
+            contextMessage += content;
+            controller.enqueue(uint8Array);
+          }
+        }
+      }
+      // for (const choice of summarizeResult.choices) {
+      //   controller.enqueue(encoder.encode("summarizing citations...\n\n"));
+      //   context.push(choice.message.content);
+      // }
+            
+      // step 3: embed context to final call
+      contextMessage += msg[msg.length - 1].content;
+      msg[msg.length - 1].content = contextMessage;
+      console.log("\n\n\nmsg\n\n", msg);
+      const events = client.listChatCompletions("plutolearning-gpt-35-turbo-16k", msg);
+
+      controller.enqueue(encoder.encode("\n\n\nResult: \n\n"));
+      for await (const event of events) {
+        for (const choice of event.choices) {
+          let content = choice.delta?.content;
+          if (content !== null && content !== undefined) { // Check if content is not null or undefined
+            // replacing the output if it contains "doc"
+            if (content.includes("doc")) {
+                content = ".";
+            } else if (content.includes("{endOfTokens}")) {
+                content = " ";
+            }
+            const uint8Array = encoder.encode(content);
+            controller.enqueue(uint8Array);
+          }
+        }
+      }
+      controller.close();
+    },
   });
-
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  
   // Respond with the stream
+  console.log('Returning StreamingTextResponse');
   return new StreamingTextResponse(stream);
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+    webpack: (config, { isServer }) => {
+      console.log(config.resolve.fallback);  // Debug line
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          "stream": require.resolve("stream-browserify"),
+        };
+      }
+      return config;
+    },
+  };
+  
