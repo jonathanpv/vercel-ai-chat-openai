9b29b9e8dd7097ef0062bce74139504414fca955
jonathanpv <jonpad512@gmail.com>
2023-10-04 15:19:27 -0500

    update server

---
+++ app/api/chat/utils.ts
@@ -0,0 +1,44 @@
+import { ChatCompletions, ChatRole } from "@azure/openai";
+
+// Define the streamable generator function
+export async function* streamableChatCompletions(stream: AsyncIterable<ChatCompletions>): AsyncGenerator<Uint8Array, void, undefined> {
+    const encoder = new TextEncoder();
+    
+    for await (const completion of stream) {
+      const text = completion.choices[0]?.delta?.content;
+      if (text) yield encoder.encode(text);
+      await new Promise(res => setTimeout(res, 30)); // Introduce a delay of 100ms between yields
+    }
+  }
+
+export function createStream(iterator: AsyncGenerator<Uint8Array, void, undefined>) {
+    return new ReadableStream({
+      async pull(controller) {
+        const { value, done } = await iterator.next();
+   
+        if (done) {
+          controller.close();
+        } else {
+          controller.enqueue(value);
+        }
+      },
+    });
+  }
+
+export async function generatorToArray(generator: AsyncGenerator<Uint8Array, void, undefined>): Promise<string[]> {
+    const array: string[] = [];
+    for await (const value of generator) {
+      array.push(new TextDecoder().decode(value));
+    }
+    return array;
+  }
+
+
+export function extractMessage(result: ChatCompletions, role: ChatRole): string {
+    for (const choice of result.choices) {
+        if(choice.message && choice.message.content && choice.message.role == role) {
+          return choice.message.content;
+        }
+      }
+    return "";
+}
