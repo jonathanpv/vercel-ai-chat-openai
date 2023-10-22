e93ec9b0006cfd1f4ff724bee643c8ce5b03abc2
Sami Ali <sami.ali@mavs.uta.edu>
2023-10-22 07:04:05 -0500

    added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {};
-
-module.exports = nextConfig;
-=======
 module.exports = {
   reactStrictMode: true,
   experimental: {
@@ -20,4 +15,3 @@ module.exports = {
     ],
   },
 };
->>>>>>> 477b9034fa55aaaf7a6ec5ba263113454f556d4e
